7a1da2d7696ccb24d43fc7eb179f8c84ecb4e7af
David Spooner <daspoon@telus.net>
2014-12-31 15:42:41 -0700

    Convert AppDelegate to swift

---
+++ Classes/AppDelegate.swift
@@ -0,0 +1,33 @@
+/*
+
+  Created by David Spooner
+
+*/
+
+import UIKit
+
+
+@UIApplicationMain
+
+
+class ColorWheelAppDelegate : UIResponder, UIApplicationDelegate
+  {
+
+    var window: UIWindow?
+
+
+    func application(sender: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
+      {
+        window = sender.keyWindow
+
+        var navigationController = UINavigationController(rootViewController: MyTableViewController(style:UITableViewStyle.Plain))
+        navigationController.toolbarHidden = false
+        navigationController.navigationBar.translucent = true
+
+        window!.rootViewController = navigationController
+        window!.makeKeyWindow()
+
+        return true
+      }
+
+  }
---
+++ ColorWheel-Bridging-Header.h
@@ -0,0 +1,13 @@
+/*
+
+  Created by David Spooner on 12/31/14.
+  Copyright (c) 2014 Lambda Software Corporation. All rights reserved.
+
+*/
+
+#ifndef ColorWheel_Bridging_Header_h
+#define ColorWheel_Bridging_Header_h
+
+#import "TableViewController.h"
+
+#endif
