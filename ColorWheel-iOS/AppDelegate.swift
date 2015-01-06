dfa4c48990ed17f9089c34006491b7247a17f024
David Spooner <daspoon@telus.net>
2015-01-05 20:46:13 -0700

    Restructured project file hierarchy; added stub for OS X version

--- ColorWheel-iOS/AppDelegate.swift
+++ ColorWheel-iOS/AppDelegate.swift
@@ -13,18 +13,16 @@ import UIKit
 class ColorWheelAppDelegate : UIResponder, UIApplicationDelegate
   {
 
-    var window: UIWindow?
+    @IBOutlet var window: UIWindow!
 
 
     func application(sender: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
       {
-        window = sender.keyWindow
-
         var navigationController = UINavigationController(rootViewController: ColorWheelTableViewController())
         navigationController.navigationBar.translucent = false
         
-        window!.rootViewController = navigationController
-        window!.makeKeyWindow()
+        window.rootViewController = navigationController
+        window.makeKeyWindow()
 
         return true
       }
--- Utilities/utilities.h
+++
@@ -1,9 +0,0 @@
-/*
-
-  Created by David Spooner on 4/14/10.
-  Copyright 2010 Lambda Software Corporation. All rights reserved.
-
-*/
-
-#import <UIKit/UIKit.h>
-
